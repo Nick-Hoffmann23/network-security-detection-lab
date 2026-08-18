8fc71ecf157098d647ae42c041c9019772604e13
Nicholas Hoffmann <nicholash1124@gmail.com>
2026-08-18 11:29:39 -0700

    Add basic port scan detection

--- src/analyze_connections.py
+++ src/analyze_connections.py
@@ -1,4 +1,6 @@
 counts = {}
+ports_by_ip = {}
+PORT_SCAN_THRESHOLD = 3
 
 with open("conn.log", "r") as f:
     for line in f:
@@ -16,3 +18,26 @@ with open("conn.log", "r") as f:
             counts[source_ip] += 1
         else:
             counts[source_ip] = 1
+
+        if source_ip not in ports_by_ip:
+            new_ports = set()
+            ports_by_ip[source_ip] = new_ports
+        
+        ports_by_ip[source_ip].add(destination_port)
+
+        
+        
+        
+
+for source_ip, count in counts.items():
+    print(source_ip, count)
+
+
+for source_ip, ports in ports_by_ip.items():
+    unique_ports = len(ports)
+
+    if unique_ports > PORT_SCAN_THRESHOLD:
+        print("Possible port scan alert:", source_ip, unique_ports, ports)
+
+    
+
